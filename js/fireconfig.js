136c0355b790369fd2408258d41b972f4f3f4b69
ManasTom <manastom670@gmail.com>
2023-11-30 17:52:55 +0530

    commit02

---
+++ js/fireconfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCd-m9eoF5D4gpGH6CNIh5fmpOFOFsCpug",
+    authDomain: "illford-task.firebaseapp.com",
+    databaseURL: "https://illford-task-default-rtdb.firebaseio.com",
+    projectId: "illford-task",
+    storageBucket: "illford-task.appspot.com",
+    messagingSenderId: "1037679498460",
+    appId: "1:1037679498460:web:291ffc01238867964a9019"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
